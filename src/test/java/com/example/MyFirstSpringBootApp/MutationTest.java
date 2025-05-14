e03703aba01569f319d6de7f019f08c9afa8ccda
Lu Luo <lu.luo@sap.com>
2025-05-13 22:27:04 -0700

    Upgrade to the latest version and format the source code.

--- src/test/java/com/example/MyFirstSpringBootApp/MutationTest.java
+++ src/test/java/com/example/MyFirstSpringBootApp/MutationTest.java
@@ -1,27 +1,22 @@
 package com.example.MyFirstSpringBootApp;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MutationTest {
 
-  @Autowired private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
   @Test
   public void smallerThanOrEqualToFiftyMessage() throws Exception {
@@ -47,6 +42,7 @@ public class MutationTest {
         .perform(get("/increment/5"))
         .andDo(print())
         .andExpect(status().isOk())
-        .andExpect(content().string("6")); // example of mutant: do not check if the value was incremented
+        .andExpect(
+            content().string("6")); // example of mutant: do not check if the value was incremented
   }
 }
